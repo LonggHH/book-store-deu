be454027d8ad799f3ed9dea00c1c28a49cf1fb66
LonggHa <hailongtam98@gmail.com>
2024-11-06 02:13:47 +0700

    lam may trang category res

--- assets/js/util.js
+++ assets/js/util.js
@@ -6,3 +6,26 @@ const DEF_URL = {
 function formatMoney(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+function throttle(func, delay) {
+    let lastCall = 0; // Biến để lưu thời gian của lần gọi gần nhất
+    return function (...args) {
+        const now = Date.now(); // Lấy thời gian hiện tại
+        if (now - lastCall >= delay) {
+            lastCall = now; // Cập nhật thời gian gọi gần nhất
+            return func.apply(this, args); // Gọi hàm với các đối số
+        }
+    };
+}
+
+// Hàm xử lý cho nút next
+function handleNext(frameId) {
+    let items = document.querySelectorAll(`#${frameId} .item`);
+    document.querySelector('.slide').appendChild(items[0]);
+}
+
+// Hàm xử lý cho nút prev
+function handlePrev(frameId) {
+    let items = document.querySelectorAll(`#${frameId} .item`);
+    document.querySelector('.slide').prepend(items[items.length - 1]);
+}
